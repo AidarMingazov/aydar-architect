4dcc941fa20334c25ef9992289797f7d0cbec2a5
Айдар Мингазов <aamingazov@gmail.com>
2016-12-23 01:27:48 +0300

    piechart init

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,12 +3,30 @@ $(document).ready(function() {
     'anchors':['1', '2', '3', '4', '5'],
     'css3': true,
     'scrollOverflow': true,
-    'navigation': true,
-    'navigationPosition': 'right',
-    'navigationTooltips': ['1', '2', '3', '4', '5'],
     menu: '.myMenu',
     'onLeave': function(index, nextIndex, direction){
       $('.myMenu').toggleClass('moveDown', index >= 1 && nextIndex > 1);
+    },
+
+    afterLoad: function(anchorLink, index){
+      if (index == 3){
+        $('.chart').easyPieChart({
+          animate: 700
+          // easing: 'easeOutBounce',
+          // animate: 700,
+          // onStep: function(from, to, percent) {
+          //     $(this.el).find('.percent').text(Math.round(percent));
+          // }
+        });
+
+        // $('.chart').easyPieChart({
+        //   easing: 'easeOutBounce',
+        //   onStep: function(from, to, percent) {
+        //     $(this.el).find('.percent').text(Math.round(percent));
+        //   }
+        // });
+
+      }
     }
   });
 });
