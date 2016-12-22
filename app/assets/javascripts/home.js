6a5df1d0dc208ebd4277f51797d3332a5099883d
Айдар Мингазов <aamingazov@gmail.com>
2016-12-22 22:56:54 +0300

    header css

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     'anchors':['1', '2', '3', '4', '5'],
     'css3': true,
     'scrollOverflow': true,
-    'sectionsColor': ['#F0F2F4', '#fff', '#fff', '#fff'],
     'navigation': true,
     'navigationPosition': 'right',
     'navigationTooltips': ['1', '2', '3', '4', '5'],
