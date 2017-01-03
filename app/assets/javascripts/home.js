95c7a5f8d4d77a836454ac5fa7e92e109ad8d71c
Айдар Мингазов <aamingazov@gmail.com>
2017-01-04 00:08:06 +0300

    portfolio

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,20 +6,24 @@ $(document).ready(function() {
     menu: '.myMenu',
     onLeave: function(index, nextIndex, direction){
       $('.myMenu').toggleClass('moveDown', index >= 1 && nextIndex > 1);
+
+      // easyPieChart init
+      if (nextIndex == 3){
+        setTimeout(function(){
+          $('.chart').easyPieChart({
+            trackColor: '#6a7a7a',
+            barColor:'#34c1c8',
+            duration: 900, enabled: true,
+            onStep: function(from, to, percent) {
+              $(this.el).find('.percent').text(Math.round(percent));
+            }
+          });
+        // easyPieChart init time
+        }, 250);
+      }
     },
     afterLoad: function(anchorLink, index){
-      if (index == 3){
-        $('.chart').easyPieChart({
-          trackColor: '#6a7a7a',
-          barColor:'#34c1c8',
-          duration: 900, enabled: true,
-          onStep: function(from, to, percent) {
-            $('.percent').text(Math.round(percent));
-          }
-        });
-      }
+
     }
   });
 });
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
+  get 'test' => 'static_pages#test'
 end
