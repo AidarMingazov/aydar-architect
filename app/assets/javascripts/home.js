36899cbd6ec4250b052c51d21bdbe033fd27cef2
Айдар Мингазов <aamingazov@gmail.com>
2017-01-04 14:15:19 +0300

    add page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    'anchors':['1', '2', '3', '4', '5'],
+    'anchors':['1', '2', '3', '4', '5', '6'],
     'css3': true,
     'scrollOverflow': true,
     menu: '.myMenu',
@@ -23,7 +23,41 @@ $(document).ready(function() {
       }
     },
     afterLoad: function(anchorLink, index){
-
+      if (index == 4){
+        // init Isotope
+        var $grid = $('.grid').isotope({
+          itemSelector: '.grid-item',
+          layoutMode: 'fitRows'
+        });
+        // filter functions
+        var filterFns = {
+          // show if number is greater than 50
+          numberGreaterThan50: function() {
+            var number = $(this).find('.number').text();
+            return parseInt( number, 10 ) > 50;
+          },
+          // show if name ends with -ium
+          ium: function() {
+            var name = $(this).find('.name').text();
+            return name.match( /ium$/ );
+          }
+        };
+        // bind filter button click
+        $('.filters-button-group').on( 'click', 'button', function() {
+          var filterValue = $( this ).attr('data-filter');
+          // use filterFn if matches value
+          filterValue = filterFns[ filterValue ] || filterValue;
+          $grid.isotope({ filter: filterValue });
+        });
+        // change is-checked class on buttons
+        $('.button-group').each( function( i, buttonGroup ) {
+          var $buttonGroup = $( buttonGroup );
+          $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+          });
+        });
+      }
     }
   });
 });
