63e7cd8c12e9f9a619c655bdcd6bd6aa22cc3ff7
Айдар Мингазов <aamingazov@gmail.com>
2017-03-29 21:55:22 +0300

    portfolio work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,34 @@ $(function(){
   }).on('autosize:resized', function(){
     $('.contact').css({"height":($(window).height()+"px")});
   });
+
+
+  // Portfolio isotope filter
+  $(window).load(function() {
+      var $container = $('.portfolio-items');
+      $container.isotope({
+          filter: '*',
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+      $('.cat a').click(function() {
+          $('.cat .active').removeClass('active');
+          $(this).addClass('active');
+          var selector = $(this).attr('data-filter');
+          $container.isotope({
+              filter: selector,
+              animationOptions: {
+                  duration: 750,
+                  easing: 'linear',
+                  queue: false
+              }
+          });
+          return false;
+      });
+  });
 });
 
 
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -41,44 +41,51 @@ $(document).ready(function() {
     },
     afterLoad: function(anchorLink, index){
       if (index == 4){
-        // init Isotope
-        var $grid = $('.grid').isotope({
-          animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-          },
-          itemSelector: '.grid-item',
-          layoutMode: 'fitRows'
-        });
-        // filter functions
-        var filterFns = {
-          // show if number is greater than 50
-          numberGreaterThan50: function() {
-            var number = $(this).find('.number').text();
-            return parseInt( number, 10 ) > 50;
-          }
-        };
-        // bind filter button click
-        $('.filters-button-group').on( 'click', 'button', function() {
-          var filterValue = $( this ).attr('data-filter');
-          // use filterFn if matches value
-          filterValue = filterFns[ filterValue ] || filterValue;
-          $grid.isotope({ filter: filterValue });
+        // // init Isotope
+        // var $grid = $('.grid').isotope({
+        //   animationOptions: {
+        //         duration: 750,
+        //         easing: 'linear',
+        //         queue: false
+        //   },
+        //   itemSelector: '.grid-item',
+        //   layoutMode: 'fitRows'
+        // });
+        // // filter functions
+        // var filterFns = {
+        //   // show if number is greater than 50
+        //   numberGreaterThan50: function() {
+        //     var number = $(this).find('.number').text();
+        //     return parseInt( number, 10 ) > 50;
+        //   }
+        // };
+        // // bind filter button click
+        // $('.filters-button-group').on( 'click', 'button', function() {
+        //   var filterValue = $( this ).attr('data-filter');
+        //   // use filterFn if matches value
+        //   filterValue = filterFns[ filterValue ] || filterValue;
+        //   $grid.isotope({ filter: filterValue });
 
-          // for footer
+        //   // for footer
+        //   $('.portfolio').css({"height":($(window).height()+"px")});
+
+        // });
+        // // change is-checked class on buttons
+        // $('.button-group').each( function( i, buttonGroup ) {
+        //   var $buttonGroup = $( buttonGroup );
+        //   $buttonGroup.on( 'click', 'button', function() {
+        //     $buttonGroup.find('.is-checked').removeClass('is-checked');
+        //     $( this ).addClass('is-checked');
+        //   });
+        // });
+
+        $(".filter").click(function () {
           $('.portfolio').css({"height":($(window).height()+"px")});
-
-        });
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-          var $buttonGroup = $( buttonGroup );
-          $buttonGroup.on( 'click', 'button', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-          });
+          return false;    
         });
       }
+
+
 
 
       // // CounterUp
